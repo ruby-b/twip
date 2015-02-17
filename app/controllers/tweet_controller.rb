679d814d44bb3d6bc5bed264ff0f4868d985ace0
sato <taaaaakashi1101@gmail.com>
2015-02-17 17:24:12 +0900

    画面ベース作成

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,4 +1,11 @@
 class TweetController < ApplicationController
   def index
+    @users = User.all
+    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+      marker.infowindow user.description
+      marker.json({title: user.title})
+    end
   end
 end
